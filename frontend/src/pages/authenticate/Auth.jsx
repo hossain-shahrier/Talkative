51d7ee22ee5dfa6604a3b8158df302ddcdca2e82
hossain-shahrier <hossainshahrier.shm@gmail.com>
2021-10-22 11:46:04 +0600

    Authentication updated

--- frontend/src/pages/authenticate/Auth.jsx
+++ frontend/src/pages/authenticate/Auth.jsx
@@ -1,5 +1,32 @@
+import { useState } from "react";
+import styled from "styled-components";
+import StepPhoneEmail from "../steps/PhoneEmailStep/StepPhoneEmail";
+import StepOtp from "../steps/OtpStep/StepOtp";
+
+const Container = styled.div`
+  display: flex;
+  position: relative;
+  top: 80px;
+  align-items: center;
+  height: 80vh;
+  width: 80vw;
+  justify-content: space-between;
+`;
+const steps = {
+  1: StepPhoneEmail,
+  2: StepOtp,
+};
 const Auth = () => {
-  return <p>kire</p>;
+  const [step, setStep] = useState(1);
+  const Step = steps[step];
+  const onNext = () => {
+    setStep(step + 1);
+  };
+  return (
+    <Container>
+      <Step onNext={onNext} />
+    </Container>
+  );
 };
 
 export default Auth;
