f083a5409a696bac007d9c14db449e7a927b79f5
hossain-shahrier <hossainshahrier.shm@gmail.com>
2021-10-27 23:23:18 +0600

    Updated Server File

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,20 @@
-require("dotenv").config();
-const express = require("express");
-
-const app = express();
 const DBConnect = require("./database");
 const router = require("./routes");
+const express = require("express");
+require("dotenv").config();
 // PORT Declaration
 const PORT = process.env.PORT || 5000;
-// Database
-DBConnect();
+
+const app = express();
+
 // JSON Middleware
 app.use(express.json());
+
+// Database Connection
+DBConnect();
+
 // Router Declaration
 app.use(router);
 
+// Server Listening
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
